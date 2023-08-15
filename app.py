ed9c8925213dc428d0828d0d2617ee2a2d98a065
nobizo <138366972+nobizo@users.noreply.github.com>
2023-08-15 13:57:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 # サイドバーに12の星座のボタンを配置します。
 st.sidebar.markdown('**星座を選択してください**')
-selected_zodiac = st.sidebar.selectbox('星座を選択してください', ['牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座', '天秤座', '蠍座', '射手座', '山羊座', '水瓶座', '魚座'])
+selected_zodiac = st.sidebar.selectbox('星座を選択してください', ['','牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座', '天秤座', '蠍座', '射手座', '山羊座', '水瓶座', '魚座'])
 
 # サイドバーの星座のボタンのどれか1つをクリックするとOpenAIのAPIにクリックした星座を渡し、OpenAIから今日の運勢を返してもらいます。
-if selected_zodiac:
+if selected_zodiac == "":
+    st.write("星座を選んでください")
+else:
     st.write(f"{selected_zodiac}の運勢です")
     
     user_message = f"今日の{selected_zodiac}の運勢を教えてください。"
@@ -19,6 +21,5 @@ if selected_zodiac:
             {"role": "user", "content": user_message}
         ]
     )
-
     bot_message = response.choices[0].message['content']
     st.write(bot_message)
