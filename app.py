c81bb676d06f03693fb2bef7e7258b4e76936ab3
nobizo <138366972+nobizo@users.noreply.github.com>
2023-08-15 14:07:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import openai
 
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
+system_prompt = """
+私は非常によく当たる有名な星占い師です。ユーザーの星座を聞くと、次の出力形式で星占いの結果を返します。
+# 出力形式
+【今日の運勢】
+【今日のラッキーカラー】
+【今日のラッキーナンバー】
+【今日のランチ】
+"""
+
 # サイドバーに12の星座のボタンを配置します。
 st.sidebar.markdown('**星座を選択してください**')
 selected_zodiac = st.sidebar.selectbox('星座を選択してください', ['','牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座', '天秤座', '蠍座', '射手座', '山羊座', '水瓶座', '魚座'])
@@ -17,7 +26,7 @@ else:
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
-            {"role": "system", "content": "You are a fortune teller."},
+            {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_message}
         ]
     )
