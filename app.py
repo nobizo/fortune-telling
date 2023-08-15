bf0039e04fc3aa9938d9cf92209da8cca3b51ca1
nobizo <138366972+nobizo@users.noreply.github.com>
2023-08-15 15:10:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import openai
+from datetime import datetime
 
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
@@ -38,39 +39,44 @@ system_prompt = """
 # selected_zodiac = st.sidebar.selectbox('星座を選択してください', ['','牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座', '天秤座', '蠍座', '射手座', '山羊座', '水瓶座', '魚座'])
 
 # サイドバーに生年月日の入力フィールドを配置します。
-selected_zodiac = ""
 st.sidebar.markdown('**生年月日を入力してください**')
-birth_date = st.sidebar.date_input('生年月日')
+
+# 選択可能な日付範囲を指定して date_input を使用します。
+min_date = datetime(1962, 10, 1)
+max_date = datetime(2005, 12, 31)
+birth_date = st.sidebar.date_input('生年月日', min_value=min_date, max_value=max_date)
+
 
 # 生年月日から星座を計算する関数
 def calculate_zodiac(birth_date):
     zodiacs = ['山羊座', '水瓶座', '魚座', '牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座', '天秤座', '蠍座', '射手座']
     day_month = birth_date.strftime('%m%d')
     
-    if (day_month >= '0120' and day_month <= '0218'):
+    if (day_month >= '1222' or day_month <= '0119'):
         return zodiacs[0]  # 山羊座
-    elif (day_month >= '0219' and day_month <= '0320'):
+    elif (day_month >= '0120' and day_month <= '0218'):
         return zodiacs[1]  # 水瓶座
-    elif (day_month >= '0321' and day_month <= '0419'):
+    elif (day_month >= '0219' and day_month <= '0320'):
         return zodiacs[2]  # 魚座
-    elif (day_month >= '0420' and day_month <= '0520'):
+    elif (day_month >= '0321' and day_month <= '0419'):
         return zodiacs[3]  # 牡羊座
-    elif (day_month >= '0521' and day_month <= '0620'):
+    elif (day_month >= '0420' and day_month <= '0520'):
         return zodiacs[4]  # 牡牛座
-    elif (day_month >= '0621' and day_month <= '0722'):
+    elif (day_month >= '0521' and day_month <= '0620'):
         return zodiacs[5]  # 双子座
-    elif (day_month >= '0723' and day_month <= '0822'):
+    elif (day_month >= '0621' and day_month <= '0722'):
         return zodiacs[6]  # 蟹座
-    elif (day_month >= '0823' and day_month <= '0922'):
+    elif (day_month >= '0723' and day_month <= '0822'):
         return zodiacs[7]  # 獅子座
-    elif (day_month >= '0923' and day_month <= '1022'):
+    elif (day_month >= '0823' and day_month <= '0922'):
         return zodiacs[8]  # 乙女座
-    elif (day_month >= '1023' and day_month <= '1121'):
+    elif (day_month >= '0923' and day_month <= '1022'):
         return zodiacs[9]  # 天秤座
-    elif (day_month >= '1122' and day_month <= '1221'):
+    elif (day_month >= '1023' and day_month <= '1121'):
         return zodiacs[10]  # 蠍座
     else:
         return zodiacs[11]  # 射手座
+
 
 # 生年月日から星座を計算
 selected_zodiac = calculate_zodiac(birth_date)
