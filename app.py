965179c1fcd6963534a636537594040de66b87df
nobizo <138366972+nobizo@users.noreply.github.com>
2023-08-15 14:56:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,47 @@ system_prompt = """
 """
 
 # サイドバーに12の星座のボタンを配置します。
-st.sidebar.markdown('**星座を選択してください**')
-selected_zodiac = st.sidebar.selectbox('星座を選択してください', ['','牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座', '天秤座', '蠍座', '射手座', '山羊座', '水瓶座', '魚座'])
+# st.sidebar.markdown('**星座を選択してください**')
+# selected_zodiac = st.sidebar.selectbox('星座を選択してください', ['','牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座', '天秤座', '蠍座', '射手座', '山羊座', '水瓶座', '魚座'])
+
+# サイドバーに生年月日の入力フィールドを配置します。
+selected_zodiac = ""
+st.sidebar.markdown('**生年月日を入力してください**')
+birth_date = st.sidebar.date_input('生年月日')
+
+# 生年月日から星座を計算する関数
+def calculate_zodiac(birth_date):
+    zodiacs = ['山羊座', '水瓶座', '魚座', '牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座', '天秤座', '蠍座', '射手座']
+    day_month = birth_date.strftime('%m%d')
+    
+    if (day_month >= '0120' and day_month <= '0218'):
+        return zodiacs[0]  # 山羊座
+    elif (day_month >= '0219' and day_month <= '0320'):
+        return zodiacs[1]  # 水瓶座
+    elif (day_month >= '0321' and day_month <= '0419'):
+        return zodiacs[2]  # 魚座
+    elif (day_month >= '0420' and day_month <= '0520'):
+        return zodiacs[3]  # 牡羊座
+    elif (day_month >= '0521' and day_month <= '0620'):
+        return zodiacs[4]  # 牡牛座
+    elif (day_month >= '0621' and day_month <= '0722'):
+        return zodiacs[5]  # 双子座
+    elif (day_month >= '0723' and day_month <= '0822'):
+        return zodiacs[6]  # 蟹座
+    elif (day_month >= '0823' and day_month <= '0922'):
+        return zodiacs[7]  # 獅子座
+    elif (day_month >= '0923' and day_month <= '1022'):
+        return zodiacs[8]  # 乙女座
+    elif (day_month >= '1023' and day_month <= '1121'):
+        return zodiacs[9]  # 天秤座
+    elif (day_month >= '1122' and day_month <= '1221'):
+        return zodiacs[10]  # 蠍座
+    else:
+        return zodiacs[11]  # 射手座
+
+# 生年月日から星座を計算
+selected_zodiac = calculate_zodiac(birth_date)
+
 
 # サイドバーの星座のボタンのどれか1つをクリックするとOpenAIのAPIにクリックした星座を渡し、OpenAIから今日の運勢を返してもらいます。
 st.image("astrologer.png")
